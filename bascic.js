0a0b7edc51b0df9518d9704d6eff3ca7f8e6b68b
johnwilfred <willjohnny25@gmail.com>
2021-04-22 17:07:40 +0530

    nodejs program

---
+++ bascic.js
@@ -0,0 +1,17 @@
+function msg(name,address){
+    console.log("hello "+ name);
+    console.log("address "+address);
+       
+}
+console.log("hellllooo");
+return msg('john','india');
+
+function msg1(salary){
+    console.log(salary);
+}
+
+return msg1("1234556");
+
+
+
+
